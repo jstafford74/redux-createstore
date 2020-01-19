065976a739355a3f641cf931d2fa8fa120bf0cad
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-01-19 08:21:32 -0500

    added hot reload example

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './components/App'
-import configureStore from './configureStore/configureStore.js'
+import configureStore from './configureStore/hotReloadStore.js'
 
 const store = configureStore()
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+const renderApp = () =>
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./components/App', renderApp)
+}
+renderApp()
